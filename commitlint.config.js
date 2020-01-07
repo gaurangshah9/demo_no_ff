1a7cb5e7ef8c87193cb10da72cc463e6d2b57530
Gaurang <gaurang.shah@mosaic.com>
2020-01-06 22:22:31 -0500

    added new files

---
+++ commitlint.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional', './commitlint.types.js'],
+    plugins: ['commitlint-plugin-jira-rules'],
+    extends: ['jira'],
+     parserPreset: './parser-preset'
+}
